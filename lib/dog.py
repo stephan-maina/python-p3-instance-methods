1aa24512df1f6699db05153c47752211636e565a
Mr.Perfect 092 <135037455+stephan-maina@users.noreply.github.com>
2023-09-05 00:11:52 +0300

    completed Lab assignment

--- lib/dog.py
+++ lib/dog.py
@@ -1,7 +1,21 @@
-#!/usr/bin/env python3
-
 class Dog:
-    # Class body goes here
+    def __init__(self, name, breed):
+        self.name = name
+        self.breed = breed
+        self.is_sitting = False
 
-    #Instance method definition
-    pass
+    def sit(self):
+        self.is_sitting = True
+        print(f"{self.name} is sitting.")
+
+    def bark(self):
+        print(f"{self.name} says Woof!")
+
+    def fetch(self, item):
+        print(f"{self.name} is fetching the {item}.")
+
+# Example usage:
+my_dog = Dog("Chess", "Golden Retriever")
+my_dog.sit()
+my_dog.bark()
+my_dog.fetch("ball")
