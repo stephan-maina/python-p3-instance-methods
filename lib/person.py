18ef7b6a7281eebb64173b7f91bf3d78c6307913
Mr.Perfect 092 <135037455+stephan-maina@users.noreply.github.com>
2023-09-05 00:13:02 +0300

    Completed Lab assignment

--- lib/person.py
+++ lib/person.py
@@ -1,7 +1,25 @@
-#!/usr/bin/env python3
-
 class Person:
-    # Class body goes here
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
 
-    #Instance method definition
-    pass
+    def talk(self):
+        print(f"{self.name} says Hello World!")
+
+    def walk(self):
+        print(f"{self.name} is walking.")
+
+    def introduce(self):
+        print(f"My name is {self.name}, and I am {self.age} years old.")
+
+# Example usage:
+person1 = Person("Vincent", 21)
+person2 = Person("Allan", 25)
+
+person1.talk()
+person1.walk()
+person1.introduce()
+
+person2.talk()
+person2.walk()
+person2.introduce()
